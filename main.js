94a87b2fadd019b68eda9064f485294f5aaecbcb
Ex Soisambat <ichsambath@gmail.com>
2019-01-11 13:47:42 +0700

    update

---
+++ main.js
@@ -0,0 +1,35 @@
+var granimInstance = new Granim({
+    element: '#canvas-basic',
+    name: 'basic-gradient',
+    direction: 'left-right',
+    opacity: [1, 1],
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#AA076B', '#61045F'],
+                ['#02AAB0', '#00CDAC'],
+                ['#DA22FF', '#9733EE']
+            ]
+        }
+    }
+});
+var typed = new Typed('.Hi', {
+    strings: ["Hello!",],
+    typeSpeed: 120,
+    showCursor: false,
+
+
+  });
+
+var typed2 = new Typed('.Dickhead', {
+    strings: ["","Dickhead!","Asshole!"],
+    typeSpeed: 120,
+    startDelay: 1200,
+    backSpeed: 20,
+    backDelay: 500,
+    showCursor: false,
+    loop:true,
+
+
+    });
