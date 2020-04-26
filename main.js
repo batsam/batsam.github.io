29a82151955a52e32c15daf4db5bb4be71dc83bc
Ex Soisambat <ichsambath@gmail.com>
2020-04-26 19:20:15 +0700

    update

--- main.js
+++ main.js
@@ -14,22 +14,22 @@ var granimInstance = new Granim({
         }
     }
 });
-var typed = new Typed('.Hi', {
-    strings: ["Her Man"],
-    typeSpeed: 120,
-    showCursor: false,
+// var typed = new Typed('.Hi', {
+//     strings: ["Her Man"],
+//     typeSpeed: 120,
+//     showCursor: false,
 
 
-  });
+//   });
 
-var typed2 = new Typed('.Words', {
-    strings: ['<a href="https://www.instagram.com/iamyourdaddyloveyou/">@iamyourdaddyloveyou</a>'],
-    typeSpeed: 120,
-    startDelay: 1200,
-    backSpeed: 20,
-    backDelay: 500,
-    showCursor: false,
-    loop:true,
+// var typed2 = new Typed('.Words', {
+//     strings: ['<a href="https://www.instagram.com/iamyourdaddyloveyou/">@iamyourdaddyloveyou</a>'],
+//     typeSpeed: 120,
+//     startDelay: 1200,
+//     backSpeed: 20,
+//     backDelay: 500,
+//     showCursor: false,
+//     loop:true,
 
 
-    });
+//     });
